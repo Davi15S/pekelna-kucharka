da9f2c816dd0de2e34b16ace1b9e6b3568485e60
Davi15S <nguyenxd072@gmail.com>
2023-01-16 19:04:29 +0100

    Fix navbar

--- src/app/layouts/Main/index.tsx
+++ src/app/layouts/Main/index.tsx
@@ -4,20 +4,35 @@ import { StaticImageData } from "next/image";
 import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import styled from "@emotion/styled";
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   const [bgImage, setBgImage] = useState<StaticImageData>();
   const [bgHeight, setBgHeight] = useState<string>();
+  const [active, setActive] = useState(false);
+
+  const handleNavbar = () => {
+    if (active) {
+      document.body.classList.remove("openNavbar");
+    } else {
+      document.body.classList.add("openNavbar");
+    }
+    setActive(!active);
+  };
 
   return (
     <PageBackgroundProvider value={{ bgImage, setBgImage, bgHeight, setBgHeight }}>
-      <div style={{ contain: "paint" }}>
-        <Navbar />
+      <PageContainer active={active}>
+        <Navbar active={active} handleNavbar={handleNavbar} />
         <Page>{children}</Page>
         <Footer />
-      </div>
+      </PageContainer>
     </PageBackgroundProvider>
   );
 }
+
+const PageContainer = styled.div<{ active: boolean }>`
+  contain: ${({ active }) => (active ? "" : "paint")};
+`;
 
 export default MainLayout;
